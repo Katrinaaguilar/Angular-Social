486e58a0aaa267dcac500f5e8b6c2cf481df3b3f
KatrinaDawnAguilar <kat15aguilar@yahoo.com>
2018-03-14 14:13:06 +0800

    added component and services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { ItemsComponent } from './components/items/items.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemsComponent
   ],
   imports: [
     BrowserModule
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCrC9jg4O81DTNGNBHA3XaU5CfWYbjN8ZM",
+    authDomain: "angular-social-d9777.firebaseapp.com",
+    databaseURL: "https://angular-social-d9777.firebaseio.com",
+    projectId: "angular-social-d9777",
+    storageBucket: "angular-social-d9777.appspot.com",
+    messagingSenderId: "570670858098"
+  }
 };
